b57c798348a6eedcf7d582b077ea238bf3ad3ac0
Kevin <kevin.linares1@utp.ac.pa>
2025-09-17 16:34:33 -0500

    Kevin Problema 2

---
+++ Problema1.py
@@ -0,0 +1,12 @@
+radio, perimetro, area, diferencia = float, float, float, float  
+
+print("Ingrese el radio de la circunferencia:")
+r = float(input())
+
+perimetro = 2 * 3.1416 * radio
+area = 3.1416 * radio * 2
+diferencia = perimetro - area
+
+print("Perimetro:", perimeter)
+print("Área:", area)
+print("Diferencia:", diferencia)
