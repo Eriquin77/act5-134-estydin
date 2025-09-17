18af6fe9c46f25c55df7aea4528f719ff021e47e
David Tulloch <david.tulloch22@gmail.com>
2025-09-17 16:26:20 -0500

    Se agrego el problema 2

---
+++ Problema 2.py
@@ -0,0 +1,6 @@
+radio = input("Introduzca el radio de la circunferencia: ")
+diametro = 2 + float(radio)
+longitud = float(radio) * 3.14
+input("Radio de la circunferencia: ", radio)
+print("Diametro de la circunferencia: ", diametro)
+print("Longitud de la circunferencia: ", longitud)
